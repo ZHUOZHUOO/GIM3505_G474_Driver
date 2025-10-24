3a4e2147d9b7ab94b95eb19bd3c21c2cba115089
ZHUOZHUOO <lzq6200066@163.com>
2025-10-24 21:39:21 +0800

    encoder_spi updata

--- Software/MDK-ARM/G474_FOC/alg_swf.d
+++
@@ -1,4 +0,0 @@
-g474_foc\alg_swf.o: USER\Algorithm\alg_swf.c
-g474_foc\alg_swf.o: USER\Algorithm\alg_swf.h
-g474_foc\alg_swf.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-g474_foc\alg_swf.o: ../MDK-ARM/USER/configure.h
